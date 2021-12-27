79b00eeeada7a4d7f175099507cc402aa4b068ce
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-12-27 20:13:00 +0100

    upload pics

---
+++ platina3000/gatsby-ssr.js
@@ -0,0 +1,10 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
+setHeadComponents([
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+]);
